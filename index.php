618652a1d31fccae6bb86aa1b61adfb7b8b6525f
alexniko01 <nikonenkoaleksandra@gmail.com>
2017-09-21 01:39:21 +0300

    #20.2

--- index.php
+++ index.php
@@ -5,15 +5,21 @@ $mysqliConnect = returnMysqliConnect();
 $now = gmdate('Y-m-d H:i:s', strtotime('now'));
 $cats = select_data($mysqliConnect, 'SELECT * FROM categories');
 
-$currentPage = $_GET['page'];
+$currentPage = 1;
+if (!empty($_GET['page'])) {
+    $currentPage = $_GET['page'];
+}
 $postPerPage = 3;
 $postsQuantity = count(select_data($mysqliConnect, "SELECT lots.id, lots.title, lots.start_price, lots.photo,lots.expire_date, categories.name AS cat FROM lots LEFT JOIN categories ON categories.id=lots.category_id WHERE expire_date > '$now'  ORDER BY lots.id ASC"));
 $pagesQuantity = ceil($postsQuantity / $postPerPage);
 $offset = ($currentPage - 1) * $postPerPage;
+$products = select_data($mysqliConnect, "SELECT lots.id, lots.title, lots.start_price, lots.photo,lots.expire_date, categories.name AS cat FROM lots LEFT JOIN categories ON categories.id=lots.category_id WHERE expire_date > '$now'  ORDER BY lots.id ASC LIMIT 3 OFFSET 0");
 
-$products = select_data($mysqliConnect, "SELECT lots.id, lots.title, lots.start_price, lots.photo,lots.expire_date, categories.name AS cat FROM lots LEFT JOIN categories ON categories.id=lots.category_id WHERE expire_date > '$now'  ORDER BY lots.id ASC LIMIT 3 OFFSET $offset");
+if (!empty($_GET['page'])) {
+    $products = select_data($mysqliConnect, "SELECT lots.id, lots.title, lots.start_price, lots.photo,lots.expire_date, categories.name AS cat FROM lots LEFT JOIN categories ON categories.id=lots.category_id WHERE expire_date > '$now'  ORDER BY lots.id ASC LIMIT 3 OFFSET $offset");
+}
 
-$pagination = getTemplate('templates/pagination.php', ['pagesQuantity' => $pagesQuantity,'currentPage' => $currentPage]);
+$pagination = getTemplate('templates/pagination.php', ['pagesQuantity' => $pagesQuantity, 'currentPage' => $currentPage]);
 
 $content = getTemplate('templates/index.php', ['cats' => $cats, 'products' => $products, 'pagination' => $pagination]);
 renderLayout($content, 'Главная', $cats);
--- templates/pagination.php
+++ templates/pagination.php
@@ -1,12 +1,18 @@
 <ul class="pagination-list">
-    <li class="pagination-item pagination-item-prev"><a href="/index.php?page=1">Назад</a></li>
+    <li class="pagination-item pagination-item-prev"><a
+                href="/index.php<?= ($currentPage != 1 && $currentPage > 2) ? '?page=' . ($currentPage - 1) : ''; ?>">Назад</a></li>
     <?php for ($i = 1; $i <= $pagesQuantity; $i++) {
         $class = '';
         if ($i == $currentPage) {
             $class = 'pagination-item-active';
-        } ?>
-        <li class="pagination-item"><a href="/index.php?page=<?= $i; ?>"><?= $i; ?></a></li>
+        };
+        $url = '/index.php';
+        if ($i != 1) {
+            $url = '/index.php?page=' . $i;
+        }
+        ?>
+        <li class="pagination-item <?= $class; ?>"><a href="<?= $url; ?>"><?= $i; ?></a></li>
     <?php } ?>
 
-    <li class="pagination-item pagination-item-next"><a href="/index.php?page=<?= $pagesQuantity; ?>">Вперед</a></li>
+    <li class="pagination-item pagination-item-next"><a href="/index.php<?= $currentPage != $pagesQuantity ? '?page=' . ($currentPage + 1) : ''; ?>">Вперед</a></li>
 </ul>
